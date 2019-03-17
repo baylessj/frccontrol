84b874def8d27262bedd4b5c39e3323475c0ade0
Jonathan Bayless <baylessj@purdue.edu>
2019-03-17 17:00:18 -0400

    Build an extension instead

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from distutils.core import setup
-from Cython.Build import cythonize
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
 setup(name='frccontrol',
-      ext_modules=cythonize("frccontrol/*.pyx", language='c++'))
+	  ext_modules=[
+    Extension('frccontrol',
+              sources=['frccontrol/ctrlutil.pyx', 'frccontrol/kalmd.pyx', 'frccontrol/lqr.pyx', 'frccontrol/models.pyx', 'frccontrol/profiles.pyx', 'frccontrol/system.pyx'],
+              # extra_compile_args=['-c -o'],
+              language='c++')
+    ],
+	cmdclass = {'build_ext': build_ext})
