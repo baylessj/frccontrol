1e91c4f0fa2a02645e2bddcf142549a926ec9ece
Jonathan Bayless <baylessj@purdue.edu>
2019-03-17 19:27:46 -0400

    Configured to work with forkner's repo

--- Makefile
+++ Makefile
@@ -7,9 +7,13 @@ quick:
 	python setup.py build_ext --inplace
 	rm *.so
 	rm -r build/*
+	mv frccontrol/*.cpp ../src/frccontrol
+	mv frccontrol/*.h ../include/frccontrol
 
 all:
 
 	python setup.py build_ext --inplace --force
 	rm *.so
 	rm -r build/*
+	mv frccontrol/*.cpp ../src/frccontrol
+	mv frccontrol/*.h ../include/frccontrol
--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+
 
 setup(name='frccontrol',
-	  ext_modules=[
-    Extension('frccontrol',
-              sources=['frccontrol/ctrlutil.pyx', 'frccontrol/kalmd.pyx', 'frccontrol/lqr.pyx', 'frccontrol/models.pyx', 'frccontrol/profiles.pyx', 'frccontrol/system.pyx'],
-              # extra_compile_args=['-c -o'],
-              language='c++')
-    ],
-	cmdclass = {'build_ext': build_ext})
+      ext_modules=cythonize("frccontrol/*.pyx", language='c++'))
